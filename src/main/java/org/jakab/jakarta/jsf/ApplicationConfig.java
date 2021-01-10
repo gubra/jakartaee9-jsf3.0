bf54d334e6e9c2c896c410aea42a52731f73fb6c
Gabor Jakab <gaborjakab@protonmail.com>
2021-01-10 13:50:01 +0100

    There is currently only one way to activate CDI in Jakarta Faces 2.3 and 3.0 and herewith make Jakarta Faces 2.3 and 3.0 to run in full Jakarta Faces 2.3/3.0 modus. Put the @FacesConfig annotation on an arbitrary CDI managed bean. For example, a general startup/configuration bean.

---
+++ src/main/java/org/jakab/jakarta/jsf/ApplicationConfig.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.jakab.jakarta.jsf;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+
+/**
+ * @author gabor
+ *
+ */
+@FacesConfig
+@ApplicationScoped
+public class ApplicationConfig {
+
+	/**
+	 * 
+	 */
+	public ApplicationConfig() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
