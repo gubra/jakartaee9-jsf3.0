4db4467f0ff4468db438fc75a182360f60b3b209
Gabor Jakab <gaborjakab@protonmail.com>
2021-01-10 13:32:50 +0100

    Init

---
+++ src/main/java/org/jakab/jakarta/jsf/TestServlet5.java
@@ -0,0 +1,52 @@
+package org.jakab.jakarta.jsf;
+
+import java.io.IOException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet5
+ */
+@WebServlet(name = "TestServlet5",urlPatterns = "/TestServlet5")
+public class TestServlet5 extends HttpServlet {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 5253395977057092045L;
+
+	/**
+     * Default constructor. 
+     */
+    public TestServlet5() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
